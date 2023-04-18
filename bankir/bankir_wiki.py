ce52d7f7dfad7951230657f6715654e782fdbfcc
Inspireed <vmeskov312@gmail.com>
2023-04-18 23:40:39 +0300

    Работает отрисовка цикла

--- bankir/bankir_wiki.py
+++ bankir/bankir_wiki.py
@@ -35,13 +35,15 @@ def bankir(processes, resources, max_resources, max_need, currently_allocated, c
     draw_all.append(copy.deepcopy(currently_allocated))
     if_not_safe1 = copy.deepcopy(currently_request)
     if_not_safe2 = copy.deepcopy(currently_allocated)
+    trans_matrix = list(zip(*if_not_safe2))
+    if_not_safe2 = copy.copy([list(row) for row in trans_matrix])
 
     null_lst = []
     running = [True] * processes
     safe = False
-    count = processes
+    count = 4
     while count != 0:
-        for i in range(processes):
+        for i in range(4):
             if running[i]:
                 executing = True
                 for j in range(resources):
@@ -64,24 +66,29 @@ def bankir(processes, resources, max_resources, max_need, currently_allocated, c
                     work = copy.copy(available)
                     finish = [0]*processes
                     is_end = True
+                    sign_not_safe = 0
                     while is_end:
                         for j in range(resources):
-                            if currently_request[finish.index(0)][j] > work[j] and finish.count(0) == 1:
+                            if sign_not_safe == 2: #currently_request[finish.index(0)][j] > work[j] and finish.count(0) == 1:
                                 is_end = False
                                 break
-                        is_ok = True
+
                         for k in range(processes):
+                            is_ok = True
                             for j in range(resources):
                                 if currently_request[k][j] > work[j]:
                                     is_ok = False
                                     break
                             if is_ok:
                                 finish[k] = 1
-                                for j in range(resources):
-                                    work[j] += currently_allocated[k][j]
-                        is_end = False
+                                if sign_not_safe == 0:
+                                    for j in range(resources):
+                                        work[j] += currently_allocated[k][j]
+                        sign_not_safe += 1
 
                     if 0 in finish:
+                        draw_req.append(copy.deepcopy(currently_request))
+                        draw_all.append(copy.deepcopy(currently_allocated))
                         #count = 0
                         currently_request = currently_request1
                         currently_allocated = currently_allocated1
@@ -91,6 +98,11 @@ def bankir(processes, resources, max_resources, max_need, currently_allocated, c
                         rest += 1
                         null_lst.append(i)
                         count -= 1
+                        # available[i] += 1
+                        # for n in range(4):
+                        #     currently_allocated[i][n] -= currently_request[i][n]
+                        #     currently_request[i][n] == 0
+
                         print(f"Процесс {i + 1} выполнен")
                         print("выделено ресурсов:")
                         [print(*currently_allocated[i]) for i in range(4)]
@@ -107,15 +119,17 @@ def bankir(processes, resources, max_resources, max_need, currently_allocated, c
         if not safe:
             label = "Система в небезопасном состоянии\n"
             print(label)
-            print(draw_all)
-            print(draw_req)
+            #print(draw_all)
+            #print(draw_req)
             draw_req = [if_not_safe1]
             draw_all = [if_not_safe2]
-            print(len(draw_all))
+            #print(len(draw_all))
+            #print(draw_all)
             rest = 1
-            # transponir
-            trans_matrix = list(zip(*draw_all[0]))
-            draw_all[0] = [list(row) for row in trans_matrix]
+            # # transponir
+            #trans_matrix = list(zip(*draw_all))
+            #draw_all = copy.copy(list(row) for row in trans_matrix)
+            print(draw_all)
 
             adj_matrix = {
                 'R1': [],
@@ -127,18 +141,18 @@ def bankir(processes, resources, max_resources, max_need, currently_allocated, c
                 'P3': [],
                 'P4': []
             }
-            for i in range(4):
-                for j in range(4):
-                    if draw_all[0][i][j] == 1:
-                        adj_matrix[f'R{i + 1}'].append(f'P{j + 1}')
-                    if draw_req[0][i][j] == 1:
-                        adj_matrix[f'P{i + 1}'].append(f'R{j + 1}')
+            for proc in range(4):
+                for x in range(4):
+                    if draw_all[0][proc][x] == 1:
+                        adj_matrix[f'R{proc + 1}'].append(f'P{x + 1}')
+                    if draw_req[0][proc][x] == 1:
+                        adj_matrix[f'P{proc + 1}'].append(f'R{x + 1}')
 
             # удаляем ключи
             to_remove = [key for key, value in adj_matrix.items() if value == []]
             for key in to_remove:
                 del adj_matrix[key]
-            # print(adj_matrix)
+            print(adj_matrix)
 
             graph = nx.DiGraph(adj_matrix)
 
@@ -149,10 +163,13 @@ def bankir(processes, resources, max_resources, max_need, currently_allocated, c
             for cycle in cycles:
                 cyc += cycle
 
-            # [['P2', 'R1'], ['R1', 'P1'], ['P1', 'R3'], ['R3', 'P2']]
-            cyc.append(cyc[0])
-            cycle_res = [[cyc[i], cyc[i + 1]] for i in range(len(cyc) - 1)]
-            # print(cycle_res)
+            #[['P2', 'R1'], ['R1', 'P1'], ['P1', 'R3'], ['R3', 'P2']]
+
+            if cyc:
+                cyc.append(cyc[0])
+                cycle_res = [[cyc[i], cyc[i+1]] for i in range(len(cyc)-1)]
+            else:
+                cycle_res = []
 
             for cycl in range(len(cycle_res)):
                 for i in range(4):
@@ -164,6 +181,7 @@ def bankir(processes, resources, max_resources, max_need, currently_allocated, c
                         for k in range(4):
                             if f'R{k + 1}' in cycle_res[cycl][1]:
                                 draw_req[0][i][k] = -1
+            #print(draw_req)
             return null_lst, label, sequence, draw_req, draw_all, rest
 
     if safe:
